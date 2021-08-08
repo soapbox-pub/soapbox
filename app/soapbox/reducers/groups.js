8f77fdd3a8bbbc2b476783c23b68c3b9b8d80aee
Alex Gleason <alex@alexgleason.me>
2021-08-08 14:00:49 -0500

    Groups: don't do that weird shit with setting group slugs to false

--- app/soapbox/reducers/groups.js
+++ app/soapbox/reducers/groups.js
@@ -1,6 +1,5 @@
 import {
   GROUP_FETCH_SUCCESS,
-  GROUP_FETCH_FAIL,
   GROUPS_FETCH_SUCCESS,
 } from '../actions/groups';
 import { GROUP_UPDATE_SUCCESS } from '../actions/group_editor';
@@ -25,8 +24,6 @@ export default function groups(state = initialState, action) {
     return normalizeGroup(state, action.group);
   case GROUPS_FETCH_SUCCESS:
     return normalizeGroups(state, action.groups);
-  case GROUP_FETCH_FAIL:
-    return state.set(action.id, false);
   default:
     return state;
   }
