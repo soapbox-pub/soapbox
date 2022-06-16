f3a1d808b7961cfa462af2182615d7510706d449
Alex Gleason <alex@alexgleason.me>
2022-06-15 19:33:50 -0500

    Danger: assign MR reviewers automatically

--- dangerfile.ts
+++ dangerfile.ts
@@ -11,3 +11,33 @@ if (docs.edited) {
 if (app.modified && !tests.modified) {
   warn('You have app changes without tests.');
 }
+
+if (danger.gitlab.metadata.repoSlug === 'soapbox-pub/soapbox-fe') {
+  const maintainers = {
+    alexgleason: 737172,
+    mkljczk: 1864889,
+    maliboomboom: 867411,
+  };
+
+  const assignReviewers = (ids: number[]) => {
+    const { CI_PROJECT_ID, CI_MERGE_REQUEST_IID } = process.env;
+
+    if (CI_PROJECT_ID && CI_MERGE_REQUEST_IID) {
+      danger.gitlab.api.MergeRequests.edit(CI_PROJECT_ID, Number(CI_MERGE_REQUEST_IID), { reviewer_ids: ids });
+    }
+  };
+
+  switch (danger.gitlab.mr.author.username) {
+    case 'alexgleason':
+      assignReviewers([maintainers.maliboomboom]);
+      break;
+    case 'mkljczk':
+      assignReviewers([maintainers.alexgleason]);
+      break;
+    case 'maliboomboom':
+      assignReviewers([maintainers.alexgleason]);
+      break;
+    default:
+      assignReviewers([maintainers.mkljczk]);
+  }
+}
