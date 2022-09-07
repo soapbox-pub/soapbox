268211615127a3c649dff50de7912a3dac896b5c
Justin <dice.warrant-02@icloud.com>
2022-09-07 08:17:11 -0400

    Update 'danger' button styles

--- app/soapbox/components/ui/button/useButtonStyles.ts
+++ app/soapbox/components/ui/button/useButtonStyles.ts
@@ -25,7 +25,7 @@ const useButtonStyles = ({
     tertiary:
       'bg-transparent border-gray-400 dark:border-gray-800 hover:border-primary-300 dark:hover:border-primary-700 focus:border-primary-500 text-gray-900 dark:text-gray-100 focus:ring-primary-500',
     accent: 'border-transparent bg-secondary-500 hover:bg-secondary-400 focus:bg-secondary-500 text-gray-100 focus:ring-secondary-300',
-    danger: 'border-transparent bg-danger-600 text-gray-100 hover:bg-danger-500 dark:hover:bg-danger-700 focus:bg-danger-600 dark:focus:bg-danger-600',
+    danger: 'border-transparent bg-danger-100 dark:bg-danger-900 text-danger-600 dark:text-danger-200 hover:bg-danger-600 hover:text-gray-100 dark:hover:text-gray-100 dark:hover:bg-danger-500 focus:bg-danger-800 dark:focus:bg-danger-600',
     transparent: 'border-transparent text-gray-800 backdrop-blur-sm bg-white/75 hover:bg-white/80',
     outline: 'border-gray-100 border-2 bg-transparent text-gray-100 hover:bg-white/10',
   };
