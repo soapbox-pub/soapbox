4ea422abb50a6d35c84ab8593858dbe6603b2928
Alex Gleason <alex@alexgleason.me>
2022-09-19 15:13:55 -0500

    ChatsPage: remove pb-36

--- app/soapbox/pages/chats-page.tsx
+++ app/soapbox/pages/chats-page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 /** Custom layout for chats on desktop. */
 const ChatsPage: React.FC = ({ children }) => {
   return (
-    <div className='md:col-span-12 lg:col-span-9 pb-36'>
+    <div className='md:col-span-12 lg:col-span-9'>
       {children}
     </div>
   );
