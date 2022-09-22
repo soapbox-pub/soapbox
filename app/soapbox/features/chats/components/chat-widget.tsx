2e728d99f9460f1e52461a57f181197464f7fed3
Alex Gleason <alex@alexgleason.me>
2022-09-22 14:36:46 -0500

    ChatWidget: remove 'direct' streaming (it uses the 'user' stream)

--- app/soapbox/features/chats/components/chat-widget.tsx
+++ app/soapbox/features/chats/components/chat-widget.tsx
@@ -1,25 +1,15 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-import { connectDirectStream } from 'soapbox/actions/streaming';
 import { ChatProvider } from 'soapbox/contexts/chat-context';
-import { useAppDispatch, useOwnAccount } from 'soapbox/hooks';
+import { useOwnAccount } from 'soapbox/hooks';
 
 import ChatPane from './chat-pane/chat-pane';
 
 const ChatWidget = () => {
   const account = useOwnAccount();
-  const dispatch = useAppDispatch();
 
   const path = location.pathname;
   const shouldHideWidget = Boolean(path.match(/^\/chats/));
-
-  useEffect(() => {
-    const disconnect = dispatch(connectDirectStream());
-
-    return (() => {
-      disconnect();
-    });
-  }, []);
 
   if (!account?.chats_onboarded || shouldHideWidget) {
     return null;
