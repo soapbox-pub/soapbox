18f73aae7c2723ee443e54bed09941398573f9f4
Alex Gleason <alex@alexgleason.me>
2022-10-31 16:50:06 -0500

    Compose: don't inherit spoilerness of post being replied to

--- app/soapbox/reducers/compose.ts
+++ app/soapbox/reducers/compose.ts
@@ -316,14 +316,6 @@ export default function compose(state = initialState, action: AnyAction) {
         map.set('caretPosition', null);
         map.set('idempotencyKey', uuid());
         map.set('content_type', defaultCompose.content_type);
-
-        if (action.status.get('spoiler_text', '').length > 0) {
-          map.set('spoiler', true);
-          map.set('spoiler_text', action.status.spoiler_text);
-        } else {
-          map.set('spoiler', false);
-          map.set('spoiler_text', '');
-        }
       }));
     case COMPOSE_QUOTE:
       return updateCompose(state, 'compose-modal', compose => compose.withMutations(map => {
