53511420285db3f2cd8f949fcd4cb2d6e84e526c
Alex Gleason <alex@alexgleason.me>
2022-11-18 17:28:56 -0600

    Fix PollForm i18n delete message

--- app/soapbox/features/compose/components/polls/poll-form.tsx
+++ app/soapbox/features/compose/components/polls/poll-form.tsx
@@ -13,8 +13,6 @@ import type { AutoSuggestion } from 'soapbox/components/autosuggest-input';
 const messages = defineMessages({
   option_placeholder: { id: 'compose_form.poll.option_placeholder', defaultMessage: 'Answer #{number}' },
   add_option: { id: 'compose_form.poll.add_option', defaultMessage: 'Add an answer' },
-  remove_option: { id: 'compose_form.poll.remove_option', defaultMessage: 'Remove this answer' }, //not used any more?
-  delete_option: { id: 'compose_form.poll.delete_option', defaultMessage: 'Delete' },
   pollDuration: { id: 'compose_form.poll.duration', defaultMessage: 'Duration' },
   removePoll: { id: 'compose_form.poll.remove', defaultMessage: 'Remove poll' },
   switchToMultiple: { id: 'compose_form.poll.switch_to_multiple', defaultMessage: 'Change poll to allow multiple answers' },
@@ -97,7 +95,7 @@ const Option: React.FC<IOption> = ({
       {index > 1 && (
         <div>
           <Button theme='danger' size='sm' onClick={handleOptionRemove}>
-            <FormattedMessage {...messages.delete_option} />
+            <FormattedMessage id='compose_form.poll.remove_option' defaultMessage='Delete' />
           </Button>
         </div>
       )}
