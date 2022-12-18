b15871aaa814ba604ddc0e960f49cdce206e6d68
Alex Gleason <alex@alexgleason.me>
2022-12-17 20:15:03 -0600

    utils/download: take a string instead of AxiosResponse

--- app/soapbox/utils/download.ts
+++ app/soapbox/utils/download.ts
@@ -1,9 +1,7 @@
-import type { AxiosResponse } from 'axios';
-
 /** Download the file from the response instead of opening it in a tab. */
 // https://stackoverflow.com/a/53230807
-export const download = (response: AxiosResponse, filename: string) => {
-  const url = URL.createObjectURL(new Blob([response.data]));
+export const download = (data: string, filename: string): void => {
+  const url = URL.createObjectURL(new Blob([data]));
   const link = document.createElement('a');
   link.href = url;
   link.setAttribute('download', filename);
