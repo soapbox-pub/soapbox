ca3f9f9d8cb4a0027014d33fbd9a15f84c9366e4
Chewbacca <dice.warrant-02@icloud.com>
2023-01-10 13:41:32 -0500

    Remove 'hasError' prop from FormGroup and Input

--- app/soapbox/features/soapbox-config/index.tsx
+++ app/soapbox/features/soapbox-config/index.tsx
@@ -39,6 +39,7 @@ const messages = defineMessages({
   customCssLabel: { id: 'soapbox_config.custom_css.meta_fields.url_placeholder', defaultMessage: 'URL' },
   rawJSONLabel: { id: 'soapbox_config.raw_json_label', defaultMessage: 'Advanced: Edit raw JSON data' },
   rawJSONHint: { id: 'soapbox_config.raw_json_hint', defaultMessage: 'Edit the settings data directly. Changes made directly to the JSON file will override the form fields above. Click "Save" to apply your changes.' },
+  rawJSONInvalid: { id: 'soapbox_config.raw_json_invalid', defaultMessage: 'is invalid' },
   verifiedCanEditNameLabel: { id: 'soapbox_config.verified_can_edit_name_label', defaultMessage: 'Allow verified users to edit their own display name.' },
   displayFqnLabel: { id: 'soapbox_config.display_fqn_label', defaultMessage: 'Display domain (eg @user@domain) for local accounts.' },
   greentextLabel: { id: 'soapbox_config.greentext_label', defaultMessage: 'Enable greentext support' },
@@ -394,11 +395,13 @@ const SoapboxConfig: React.FC = () => {
             expanded={jsonEditorExpanded}
             onToggle={toggleJSONEditor}
           >
-            <FormGroup hintText={intl.formatMessage(messages.rawJSONHint)}>
+            <FormGroup
+              hintText={intl.formatMessage(messages.rawJSONHint)}
+              errors={jsonValid ? undefined : [intl.formatMessage(messages.rawJSONInvalid)]}
+            >
               <Textarea
                 value={rawJSON}
                 onChange={handleEditJSON}
-                hasError={!jsonValid}
                 isCodeEditor
                 rows={12}
               />
