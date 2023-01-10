4a345d71272492cc41b78aa1584592f4aca0e7f1
Chewbacca <dice.warrant-02@icloud.com>
2023-01-10 13:21:29 -0500

    Ensure notifications are enabled

--- app/soapbox/actions/notifications.ts
+++ app/soapbox/actions/notifications.ts
@@ -107,7 +107,10 @@ const updateNotificationsQueue = (notification: APIEntity, intlMessages: Record<
 
     // Desktop notifications
     try {
-      if (showAlert && !filtered) {
+      // eslint-disable-next-line compat/compat
+      const isNotificationsEnabled = typeof window.Notification !== 'undefined' && Notification.permission === 'granted';
+
+      if (showAlert && !filtered && isNotificationsEnabled) {
         const title = new IntlMessageFormat(intlMessages[`notification.${notification.type}`], intlLocale).format({ name: notification.account.display_name.length > 0 ? notification.account.display_name : notification.account.username });
         const body = (notification.status && notification.status.spoiler_text.length > 0) ? notification.status.spoiler_text : unescapeHTML(notification.status ? notification.status.content : '');
 
