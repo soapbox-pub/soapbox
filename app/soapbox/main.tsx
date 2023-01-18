feeb80fe227eb9d4b01a9d3119e5fd07d399fea9
Alex Gleason <alex@alexgleason.me>
2023-01-17 21:37:00 -0600

    Remove marky, mark-loader

--- app/soapbox/main.tsx
+++ app/soapbox/main.tsx
@@ -25,7 +25,6 @@ import '../styles/application.scss';
 import './precheck';
 import { default as Soapbox } from './containers/soapbox';
 import * as monitoring from './monitoring';
-import * as perf from './performance';
 import ready from './ready';
 import toast from './toast';
 
@@ -33,8 +32,6 @@ const messages = defineMessages({
   update: { id: 'sw.update', defaultMessage: 'Update' },
   updateText: { id: 'sw.update_text', defaultMessage: 'An update is available.' },
 });
-
-perf.start('main()');
 
 // Sentry
 monitoring.start();
@@ -69,5 +66,4 @@ ready(() => {
       },
     });
   }
-  perf.stop('main()');
 });
