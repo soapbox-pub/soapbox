6543bf440f1d7e4e56fa56162d97bf263eea5bda
Alex Gleason <alex@alexgleason.me>
2023-01-18 11:40:33 -0600

    Vite: preliminary setup

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import path from 'path';
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import vitePluginRequire from 'vite-plugin-require';
+
+export default defineConfig({
+  root: 'app',
+  build: {
+    // Relative to the root
+    outDir: '../static',
+  },
+  plugins: [
+    createHtmlPlugin({
+      template: 'index.html',
+    }),
+    react({
+      // Use React plugin in all *.jsx and *.tsx files
+      include: '**/*.{jsx,tsx}',
+    }),
+    // @ts-ignore
+    vitePluginRequire.default(),
+  ],
+  resolve: {
+    alias: [
+      { find: 'soapbox', replacement: path.resolve(__dirname, 'app', 'soapbox') },
+      { find: 'assets', replacement: path.resolve(__dirname, 'app', 'assets') },
+    ],
+  },
+  assetsInclude: ['**/*.oga'],
+});
