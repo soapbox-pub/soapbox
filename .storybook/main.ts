b6df6149f408b9b3b676368d048526b11860907f
Alex Gleason <alex@alexgleason.me>
2023-02-02 20:21:13 -0600

    Convert Storybook config to TypeScript

---
+++ .storybook/main.ts
@@ -0,0 +1,19 @@
+import type { StorybookConfig } from '@storybook/core-common';
+
+const config: StorybookConfig = {
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)'
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+};
+
+export default config;
