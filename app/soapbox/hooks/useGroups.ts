8923e7b5d093e425def0063242d2931ef0896930
Alex Gleason <alex@alexgleason.me>
2023-03-09 11:23:14 -0600

    Combine group hooks into one useGroups file

--- app/soapbox/hooks/useGroups.ts
+++ app/soapbox/hooks/useGroups.ts
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 
-import { useEntities } from 'soapbox/entity-store/hooks';
+import { useEntities, useEntity } from 'soapbox/entity-store/hooks';
 import { normalizeGroup } from 'soapbox/normalizers';
 
 import type { Group } from 'soapbox/types/entities';
@@ -9,6 +9,8 @@ function useGroups() {
   const result = useEntities<Group>(['Group', ''], '/api/v1/groups');
   const { entities, isLoading, fetchEntities } = result;
 
+  // Note: we have to fetch them in the hook right now because I haven't implemented
+  // max-age or cache expiry in the entity store yet. It's planned.
   useEffect(() => {
     if (!isLoading) {
       fetchEntities();
@@ -21,4 +23,20 @@ function useGroups() {
   };
 }
 
-export { useGroups };
+function useGroup(groupId: string) {
+  const result = useEntity<Group>(['Group', groupId], `/api/v1/groups/${groupId}`);
+  const { entity, isLoading, fetchEntity } = result;
+
+  useEffect(() => {
+    if (!isLoading) {
+      fetchEntity();
+    }
+  }, []);
+
+  return {
+    ...result,
+    group: entity ? normalizeGroup(entity) : undefined,
+  };
+}
+
+export { useGroup, useGroups };
