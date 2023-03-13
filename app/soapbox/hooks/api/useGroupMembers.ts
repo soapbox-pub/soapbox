4380357a66589618fa6476b325286f8ae34d02cf
Chewbacca <dice.warrant-02@icloud.com>
2023-03-13 15:08:02 -0400

    Use ZOD for group-members

--- app/soapbox/hooks/api/useGroupMembers.ts
+++ app/soapbox/hooks/api/useGroupMembers.ts
@@ -1,28 +1,17 @@
 import { Entities } from 'soapbox/entity-store/entities';
 import { useEntities } from 'soapbox/entity-store/hooks';
-import { normalizeAccount } from 'soapbox/normalizers';
-import { Account } from 'soapbox/types/entities';
+import { GroupMember, groupMemberSchema } from 'soapbox/schemas';
 
-import { BaseGroupRoles, TruthSocialGroupRoles } from '../useGroupRoles';
-
-interface GroupMember {
-  id: string
-  role: BaseGroupRoles | TruthSocialGroupRoles
-  account: Account | any
-}
-
-const normalizeGroupMember = (groupMember: GroupMember): GroupMember => {
-  return {
-    ...groupMember,
-    account: normalizeAccount(groupMember.account),
-  };
+const parseGroupMember = (entity: unknown) => {
+  const result = groupMemberSchema.safeParse(entity);
+  if (result.success) {
+    return result.data;
+  }
 };
-
-const parseGroupMember = (entity: unknown) => entity ? normalizeGroupMember(entity as GroupMember) : undefined;
 
 function useGroupMembers(groupId: string, role: string) {
   const { entities, ...result } = useEntities<GroupMember>(
-    [Entities.GROUP_MEMBERSHIPS, `${groupId}:${role}`],
+    [Entities.GROUP_MEMBERSHIPS, groupId, role],
     `/api/v1/groups/${groupId}/memberships?role=${role}&limit=1`,
     { parser: parseGroupMember },
   );
--- app/soapbox/schemas/index.ts
+++ app/soapbox/schemas/index.ts
@@ -1,3 +1,4 @@
 export { customEmojiSchema, CustomEmoji } from './custom-emoji';
 export { groupSchema, Group } from './group';
+export { groupMemberSchema, GroupMember } from './group-member';
 export { groupRelationshipSchema, GroupRelationship } from './group-relationship';
