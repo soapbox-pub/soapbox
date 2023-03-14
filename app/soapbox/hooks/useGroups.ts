12e0b8194784939c9040eadfd2b47a0b616a2a02
Chewbacca <dice.warrant-02@icloud.com>
2023-03-14 10:19:34 -0400

    Use Entities enum

--- app/soapbox/hooks/useGroups.ts
+++ app/soapbox/hooks/useGroups.ts
@@ -1,12 +1,20 @@
+import { Entities } from 'soapbox/entity-store/entities';
 import { useEntities, useEntity } from 'soapbox/entity-store/hooks';
 import { groupSchema, Group } from 'soapbox/schemas/group';
 import { groupRelationshipSchema, GroupRelationship } from 'soapbox/schemas/group-relationship';
 
 function useGroups() {
-  const { entities, ...result } = useEntities<Group>(['Group', ''], '/api/v1/groups', { schema: groupSchema });
+  const { entities, ...result } = useEntities<Group>(
+    [Entities.GROUPS, ''],
+    '/api/v1/groups',
+    { schema: groupSchema },
+  );
   const { relationships } = useGroupRelationships(entities.map(entity => entity.id));
 
-  const groups = entities.map((group) => ({ ...group, relationship: relationships[group.id] || null }));
+  const groups = entities.map((group) => ({
+    ...group,
+    relationship: relationships[group.id] || null,
+  }));
 
   return {
     ...result,
@@ -15,7 +23,11 @@ function useGroups() {
 }
 
 function useGroup(groupId: string, refetch = true) {
-  const { entity: group, ...result } = useEntity<Group>(['Group', groupId], `/api/v1/groups/${groupId}`, { schema: groupSchema, refetch });
+  const { entity: group, ...result } = useEntity<Group>(
+    [Entities.GROUPS, groupId],
+    `/api/v1/groups/${groupId}`,
+    { schema: groupSchema, refetch },
+  );
   const { entity: relationship } = useGroupRelationship(groupId);
 
   return {
@@ -25,13 +37,21 @@ function useGroup(groupId: string, refetch = true) {
 }
 
 function useGroupRelationship(groupId: string) {
-  return useEntity<GroupRelationship>(['GroupRelationship', groupId], `/api/v1/groups/relationships?id[]=${groupId}`, { schema: groupRelationshipSchema });
+  return useEntity<GroupRelationship>(
+    [Entities.GROUP_RELATIONSHIPS, groupId],
+    `/api/v1/groups/relationships?id[]=${groupId}`,
+    { schema: groupRelationshipSchema },
+  );
 }
 
 function useGroupRelationships(groupIds: string[]) {
   const q = groupIds.map(id => `id[]=${id}`).join('&');
   const endpoint = groupIds.length ? `/api/v1/groups/relationships?${q}` : undefined;
-  const { entities, ...result } = useEntities<GroupRelationship>(['GroupRelationship', q], endpoint, { schema: groupRelationshipSchema });
+  const { entities, ...result } = useEntities<GroupRelationship>(
+    [Entities.GROUP_RELATIONSHIPS, q],
+    endpoint,
+    { schema: groupRelationshipSchema },
+  );
 
   const relationships = entities.reduce<Record<string, GroupRelationship>>((map, relationship) => {
     map[relationship.id] = relationship;
