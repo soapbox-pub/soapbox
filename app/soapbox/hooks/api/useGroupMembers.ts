1d53f48904118b1d5c4b54228b30834aa0b8a701
Chewbacca <dice.warrant-02@icloud.com>
2023-03-14 12:55:42 -0400

    Fix parser

--- app/soapbox/hooks/api/useGroupMembers.ts
+++ app/soapbox/hooks/api/useGroupMembers.ts
@@ -2,18 +2,11 @@ import { Entities } from 'soapbox/entity-store/entities';
 import { useEntities } from 'soapbox/entity-store/hooks';
 import { GroupMember, groupMemberSchema } from 'soapbox/schemas';
 
-const parseGroupMember = (entity: unknown) => {
-  const result = groupMemberSchema.safeParse(entity);
-  if (result.success) {
-    return result.data;
-  }
-};
-
 function useGroupMembers(groupId: string, role: string) {
   const { entities, ...result } = useEntities<GroupMember>(
     [Entities.GROUP_MEMBERSHIPS, groupId, role],
-    `/api/v1/groups/${groupId}/memberships?role=${role}&limit=1`,
-    { parser: parseGroupMember },
+    `/api/v1/groups/${groupId}/memberships?role=${role}`,
+    { schema: groupMemberSchema },
   );
 
   return {
