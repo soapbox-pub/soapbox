b87af6a71c6297e26bee4cab8eb1348f818dbacb
Alex Gleason <alex@alexgleason.me>
2023-03-20 20:11:21 -0500

    AuthorizeRejectButtons: fix styles, make less fragile

--- app/soapbox/components/authorize-reject-buttons.tsx
+++ app/soapbox/components/authorize-reject-buttons.tsx
@@ -37,15 +37,15 @@ const AuthorizeRejectButtons: React.FC<IAuthorizeRejectButtons> = ({ onAuthorize
           <IconButton
             src={require('@tabler/icons/x.svg')}
             onClick={handleReject}
-            theme='outlined'
-            className='h-10 w-10 items-center justify-center border-2 border-danger-600/10 hover:border-danger-600 dark:border-danger-600/10 dark:hover:border-danger-600'
+            theme='seamless'
+            className='h-10 w-10 items-center justify-center border-2 border-danger-600/10 hover:border-danger-600'
             iconClassName='h-6 w-6 text-danger-600'
           />
           <IconButton
             src={require('@tabler/icons/check.svg')}
             onClick={handleAuthorize}
-            theme='outlined'
-            className='h-10 w-10 items-center justify-center border-2 border-primary-500/10 hover:border-primary-500 dark:border-primary-500/10 dark:hover:border-primary-500'
+            theme='seamless'
+            className='h-10 w-10 items-center justify-center border-2 border-primary-500/10 hover:border-primary-500'
             iconClassName='h-6 w-6 text-primary-500'
           />
         </HStack>
