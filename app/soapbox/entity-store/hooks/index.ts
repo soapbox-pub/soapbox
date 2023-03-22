d2fd9e03876b55d40c0c8a856a2395cb0bfcaedd
Alex Gleason <alex@alexgleason.me>
2023-03-22 15:32:56 -0500

    Export new entity hooks

--- app/soapbox/entity-store/hooks/index.ts
+++ app/soapbox/entity-store/hooks/index.ts
@@ -1,3 +1,6 @@
 export { useEntities } from './useEntities';
 export { useEntity } from './useEntity';
 export { useEntityActions } from './useEntityActions';
+export { useCreateEntity } from './useCreateEntity';
+export { useDeleteEntity } from './useDeleteEntity';
+export { useDismissEntity } from './useDismissEntity';
