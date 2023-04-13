8702c169988461a62a3673845fa14e2f902c2729
Chewbacca <dice.warrant-02@icloud.com>
2023-04-13 07:34:37 -0400

    Update GroupTag schema

--- app/soapbox/schemas/group-tag.ts
+++ app/soapbox/schemas/group-tag.ts
@@ -2,11 +2,11 @@ import z from 'zod';
 
 const groupTagSchema = z.object({
   id: z.string(),
-  uses: z.number(),
   name: z.string(),
-  url: z.string(),
-  pinned: z.boolean().catch(false),
-  visible: z.boolean().default(true),
+  uses: z.number().optional(),
+  url: z.string().optional(),
+  pinned: z.boolean().optional().catch(false),
+  visible: z.boolean().optional().default(true),
 });
 
 type GroupTag = z.infer<typeof groupTagSchema>;
