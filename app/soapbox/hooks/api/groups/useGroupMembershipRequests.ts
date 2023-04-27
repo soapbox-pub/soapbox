d8a3f51e4a95c791e9b17790d36b1377433f5916
Alex Gleason <alex@alexgleason.me>
2023-04-27 14:11:45 -0500

    useGroupMembershipRequests: dismiss instead of decrement

--- app/soapbox/hooks/api/groups/useGroupMembershipRequests.ts
+++ app/soapbox/hooks/api/groups/useGroupMembershipRequests.ts
@@ -1,5 +1,5 @@
 import { Entities } from 'soapbox/entity-store/entities';
-import { useEntities, useIncrementEntity } from 'soapbox/entity-store/hooks';
+import { useDismissEntity, useEntities } from 'soapbox/entity-store/hooks';
 import { useApi } from 'soapbox/hooks/useApi';
 import { accountSchema } from 'soapbox/schemas';
 import { GroupRoles } from 'soapbox/schemas/group-member';
@@ -23,13 +23,13 @@ function useGroupMembershipRequests(groupId: string) {
     },
   );
 
-  const { incrementEntity: authorize } = useIncrementEntity(path, -1, async (accountId: string) => {
+  const { dismissEntity: authorize } = useDismissEntity(path, async (accountId: string) => {
     const response = await api.post(`/api/v1/groups/${groupId}/membership_requests/${accountId}/authorize`);
     invalidate();
     return response;
   });
 
-  const { incrementEntity: reject } = useIncrementEntity(path, -1, async (accountId: string) => {
+  const { dismissEntity: reject } = useDismissEntity(path, async (accountId: string) => {
     const response = await api.post(`/api/v1/groups/${groupId}/membership_requests/${accountId}/reject`);
     invalidate();
     return response;
