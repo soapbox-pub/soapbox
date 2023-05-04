a7e1350a6505a700b3b23c5d58fd0b6c20dedd59
Alex Gleason <alex@alexgleason.me>
2023-05-04 11:31:58 -0500

    Add real chatMessageSchema

--- app/soapbox/schemas/chat-message.ts
+++ app/soapbox/schemas/chat-message.ts
@@ -1,9 +1,25 @@
 import { z } from 'zod';
 
-import { normalizeChatMessage } from 'soapbox/normalizers';
-import { toSchema } from 'soapbox/utils/normalizers';
+import { attachmentSchema } from './attachment';
+import { cardSchema } from './card';
+import { customEmojiSchema } from './custom-emoji';
+import { emojiSchema, filteredArray } from './utils';
 
-const chatMessageSchema = toSchema(normalizeChatMessage);
+const chatMessageSchema = z.object({
+  account_id: z.string(),
+  media_attachments: filteredArray(attachmentSchema),
+  card: cardSchema.nullable().catch(null),
+  chat_id: z.string(),
+  content: z.string().catch(''),
+  created_at: z.string().datetime().catch(new Date().toUTCString()),
+  emojis: filteredArray(customEmojiSchema),
+  expiration: z.number().optional().catch(undefined),
+  emoji_reactions: z.array(emojiSchema).min(1).nullable().catch(null),
+  id: z.string(),
+  unread: z.coerce.boolean(),
+  deleting: z.coerce.boolean(),
+  pending: z.coerce.boolean(),
+});
 
 type ChatMessage = z.infer<typeof chatMessageSchema>;
 
