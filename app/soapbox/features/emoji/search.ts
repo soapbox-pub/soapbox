1fed96e99e0fdc37e4fdd443057bef81408430d4
Chewbacca <dice.warrant-02@icloud.com>
2023-06-05 12:39:32 -0400

    Sort the emojis

--- app/soapbox/features/emoji/search.ts
+++ app/soapbox/features/emoji/search.ts
@@ -11,8 +11,10 @@ const index = new Index({
   context: true,
 });
 
-for (const [key, emoji] of Object.entries(data.emojis)) {
-  index.add('n' + key, `${emoji.keywords.join(' ')} ${emoji.name}`);
+// console.log(Object.entries(data.emojis));
+const sortedEmojis = Object.entries(data.emojis).sort((a, b) => a[0].localeCompare(b[0]));
+for (const [key, emoji] of sortedEmojis) {
+  index.add('n' + key, `${emoji.id} ${emoji.name} ${emoji.keywords.join(' ')}`);
 }
 
 export interface searchOptions {
