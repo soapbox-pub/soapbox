89c9e32b5932bfda44da201aaf1e4ea633711522
Alex Gleason <alex@alexgleason.me>
2023-06-19 16:02:51 -0500

    Move legacy functions into separate utils file

--- app/soapbox/reducers/index.ts
+++ app/soapbox/reducers/index.ts
@@ -1,5 +1,4 @@
 import { Record as ImmutableRecord } from 'immutable';
-import { default as lodashGet } from 'lodash/get';
 import { combineReducers } from 'redux-immutable';
 import { createSelector } from 'reselect';
 
@@ -7,6 +6,7 @@ import { AUTH_LOGGED_OUT } from 'soapbox/actions/auth';
 import * as BuildConfig from 'soapbox/build-config';
 import { Entities } from 'soapbox/entity-store/entities';
 import entities from 'soapbox/entity-store/reducer';
+import { immutableizeStore } from 'soapbox/utils/legacy';
 
 import account_notes from './account-notes';
 import accounts_counters from './accounts-counters';
@@ -74,65 +74,6 @@ import verification from './verification';
 import type { AnyAction, Reducer } from 'redux';
 import type { EntityStore } from 'soapbox/entity-store/types';
 import type { Account } from 'soapbox/schemas';
-
-interface LegacyMap {
-  get(key: any): unknown
-  getIn(keyPath: any[]): unknown
-  toJS(): any
-}
-
-interface LegacyStore<T> extends LegacyMap {
-  get(key: any): T & LegacyMap | undefined
-  getIn(keyPath: any[]): unknown
-  find(predicate: (value: T & LegacyMap, key: string) => boolean): T & LegacyMap | undefined
-  filter(predicate: (value: T & LegacyMap, key: string) => boolean): (T & LegacyMap)[]
-}
-
-function immutableizeEntity<T extends Record<any, any>>(entity: T): T & LegacyMap {
-  return {
-    ...entity,
-
-    get(key: any): unknown {
-      return entity[key];
-    },
-
-    getIn(keyPath: any[]): unknown {
-      return lodashGet(entity, keyPath);
-    },
-
-    toJS() {
-      return entity;
-    },
-  };
-}
-
-function immutableizeStore<T, S extends Record<string, T | undefined>>(state: S): S & LegacyStore<T> {
-  return {
-    ...state,
-
-    get(id: any): T & LegacyMap | undefined {
-      const entity = state[id];
-      return entity ? immutableizeEntity(entity) : undefined;
-    },
-
-    getIn(keyPath: any[]): unknown {
-      return lodashGet(state, keyPath);
-    },
-
-    find(predicate: (value: T & LegacyMap, key: string) => boolean): T & LegacyMap | undefined {
-      const result = Object.entries(state).find(([key, value]) => value && predicate(immutableizeEntity(value), key))?.[1];
-      return result ? immutableizeEntity(result) : undefined;
-    },
-
-    filter(predicate: (value: T & LegacyMap, key: string) => boolean): (T & LegacyMap)[] {
-      return Object.entries(state).filter(([key, value]) => value && predicate(immutableizeEntity(value), key)).map(([key, value]) => immutableizeEntity(value!));
-    },
-
-    toJS() {
-      return state;
-    },
-  };
-}
 
 const reducers = {
   account_notes,
