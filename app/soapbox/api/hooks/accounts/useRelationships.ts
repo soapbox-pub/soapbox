ab5c4a423350ba2f746eba0ceb75695521e31d45
Alex Gleason <alex@alexgleason.me>
2023-06-23 11:30:10 -0500

    Fix useRelationships getting called while logged out

--- app/soapbox/api/hooks/accounts/useRelationships.ts
+++ app/soapbox/api/hooks/accounts/useRelationships.ts
@@ -1,15 +1,17 @@
 import { Entities } from 'soapbox/entity-store/entities';
 import { useEntities } from 'soapbox/entity-store/hooks';
+import { useLoggedIn } from 'soapbox/hooks';
 import { useApi } from 'soapbox/hooks/useApi';
 import { type Relationship, relationshipSchema } from 'soapbox/schemas';
 
 function useRelationships(ids: string[]) {
   const api = useApi();
+  const { isLoggedIn } = useLoggedIn();
 
   const { entities: relationships, ...result } = useEntities<Relationship>(
     [Entities.RELATIONSHIPS],
     () => api.get(`/api/v1/accounts/relationships?${ids.map(id => `id[]=${id}`).join('&')}`),
-    { schema: relationshipSchema, enabled: ids.filter(Boolean).length > 0 },
+    { schema: relationshipSchema, enabled: isLoggedIn && ids.filter(Boolean).length > 0 },
   );
 
   return {
