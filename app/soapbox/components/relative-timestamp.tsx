f518a7e5e450eae36a93a076bc2c7a7155dc7436
marcin mikołajczak <git@mkljczk.pl>
2023-08-28 19:58:11 +0200

    Add title to created_at label
    Signed-off-by: marcin mikołajczak <git@mkljczk.pl>

--- app/soapbox/components/relative-timestamp.tsx
+++ app/soapbox/components/relative-timestamp.tsx
@@ -16,7 +16,7 @@ const messages = defineMessages({
   days_remaining: { id: 'time_remaining.days', defaultMessage: '{number, plural, one {# day} other {# days}} left' },
 });
 
-const dateFormatOptions: FormatDateOptions = {
+export const dateFormatOptions: FormatDateOptions = {
   hour12: true,
   year: 'numeric',
   month: 'short',
