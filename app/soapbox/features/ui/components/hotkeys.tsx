e83351f37641215765edf1ad17b9fbf19eb437a7
Alex Gleason <alex@alexgleason.me>
2023-09-14 13:04:14 -0500

    Temporarily disable hotkeys

--- app/soapbox/features/ui/components/hotkeys.tsx
+++ app/soapbox/features/ui/components/hotkeys.tsx
@@ -6,13 +6,17 @@ type IHotKeys = React.ComponentProps<typeof _HotKeys>;
 /**
  * Wrapper component around `react-hotkeys`.
  * `react-hotkeys` is a legacy component, so confining its import to one place is beneficial.
+ *
+ * NOTE: Temporarily disabled due to incompatibility with Vite.
  */
 const HotKeys = React.forwardRef<any, IHotKeys>(({ children, ...rest }, ref) => {
-  return (
-    <_HotKeys {...rest} ref={ref}>
-      {children}
-    </_HotKeys>
-  );
+  // return (
+  //   <_HotKeys {...rest} ref={ref}>
+  //     {children}
+  //   </_HotKeys>
+  // );
+
+  return <>{children}</>;
 });
 
 export { HotKeys, type IHotKeys };
