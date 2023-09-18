2b11b579a32fe72053997a1ed53131613959232d
Alex Gleason <alex@alexgleason.me>
2023-09-18 17:39:22 -0500

    Move index.html to project root

--- vite.config.ts
+++ vite.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
     vitePluginRequire.default(),
     compileTime(),
     createHtmlPlugin({
-      template: 'src/index.html',
+      template: 'index.html',
       minify: {
         collapseWhitespace: true,
         removeComments: false,
