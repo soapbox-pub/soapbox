e712d7bb5270cf059129d30355462e3ec5fb7289
Alex Gleason <alex@alexgleason.me>
2023-09-23 16:26:46 -0500

    lexical: remove unused nodes

--- src/features/compose/editor/nodes/index.ts
+++ src/features/compose/editor/nodes/index.ts
@@ -4,46 +4,21 @@
  * LICENSE file in the /app/soapbox/features/compose/editor directory.
  */
 
-import { CodeHighlightNode, CodeNode } from '@lexical/code';
 import { HashtagNode } from '@lexical/hashtag';
-import { AutoLinkNode, LinkNode } from '@lexical/link';
-import { ListItemNode, ListNode } from '@lexical/list';
-import { HorizontalRuleNode } from '@lexical/react/LexicalHorizontalRuleNode';
-import { HeadingNode, QuoteNode } from '@lexical/rich-text';
-
-import { useFeatures, useInstance } from 'soapbox/hooks';
+import { AutoLinkNode } from '@lexical/link';
 
 import { EmojiNode } from './emoji-node';
-import { ImageNode } from './image-node';
 import { MentionNode } from './mention-node';
 
 import type { Klass, LexicalNode } from 'lexical';
 
 const useNodes = () => {
-  const features = useFeatures();
-  const instance = useInstance();
-
   const nodes: Array<Klass<LexicalNode>> = [
     AutoLinkNode,
     HashtagNode,
     EmojiNode,
     MentionNode,
   ];
-
-  if (features.richText) {
-    nodes.push(
-      CodeHighlightNode,
-      CodeNode,
-      HorizontalRuleNode,
-      LinkNode,
-      ListItemNode,
-      ListNode,
-      QuoteNode,
-    );
-  }
-
-  if (instance.pleroma.getIn(['metadata', 'markup', 'allow_headings'])) nodes.push(HeadingNode);
-  if (instance.pleroma.getIn(['metadata', 'markup', 'allow_inline_images'])) nodes.push(ImageNode);
 
   return nodes;
 };
