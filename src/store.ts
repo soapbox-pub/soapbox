bcf3f4e01d49fa886aa7f199fb2c9bd03056b763
Alex Gleason <alex@alexgleason.me>
2023-12-20 22:06:49 -0600

    Upgrade all the Redux things

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,5 @@
-import { configureStore } from '@reduxjs/toolkit';
-import thunk, { ThunkDispatch } from 'redux-thunk';
+import { configureStore, Tuple } from '@reduxjs/toolkit';
+import { thunk, type ThunkDispatch } from 'redux-thunk';
 
 import errorsMiddleware from './middleware/errors';
 import soundsMiddleware from './middleware/sounds';
@@ -9,11 +9,11 @@ import type { AnyAction } from 'redux';
 
 export const store = configureStore({
   reducer: appReducer,
-  middleware: [
+  middleware: () => new Tuple(
     thunk,
     errorsMiddleware(),
     soundsMiddleware(),
-  ],
+  ),
   devTools: true,
 });
 
