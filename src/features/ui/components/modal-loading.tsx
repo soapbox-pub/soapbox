623fb234b368286434d8ffecaa2c7e187dc5b21b
Alex Gleason <alex@alexgleason.me>
2024-02-19 14:28:43 -0600

    Fix ModalLoading component

--- src/features/ui/components/modal-loading.tsx
+++ src/features/ui/components/modal-loading.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
 
-import { Spinner } from 'soapbox/components/ui';
+import { Modal, Spinner } from 'soapbox/components/ui';
 
 const ModalLoading = () => (
-  <div className='modal-root__modal error-modal'>
-    <div className='error-modal__body'>
-      <Spinner />
-    </div>
-    <div className='error-modal__footer'>
-      <div>
-        <button className='error-modal__nav' />
-      </div>
-    </div>
-  </div>
+  <Modal>
+    <Spinner />
+  </Modal>
 );
 
 export default ModalLoading;
