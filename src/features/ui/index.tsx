1ef88e2cc242fae0c515d4e4107f11372dd80ac1
marcin mikołajczak <git@mkljczk.pl>
2024-03-23 15:59:24 +0100

    Bookmark folders route should not be feature gated
    Signed-off-by: marcin mikołajczak <git@mkljczk.pl>

--- src/features/ui/index.tsx
+++ src/features/ui/index.tsx
@@ -246,7 +246,7 @@ const SwitchingColumnsArea: React.FC<ISwitchingColumnsArea> = ({ children }) => 
       {features.lists && <WrappedRoute path='/list/:id' page={DefaultPage} component={ListTimeline} content={children} />}
       {features.bookmarks && <WrappedRoute path='/bookmarks/all' page={DefaultPage} component={Bookmarks} content={children} />}
       {features.bookmarks && <WrappedRoute path='/bookmarks/:id' page={DefaultPage} component={Bookmarks} content={children} />}
-      {features.bookmarkFolders && <WrappedRoute path='/bookmarks' page={DefaultPage} component={BookmarkFolders} content={children} />}
+      <WrappedRoute path='/bookmarks' page={DefaultPage} component={BookmarkFolders} content={children} />
 
       <WrappedRoute path='/notifications' page={DefaultPage} component={Notifications} content={children} />
 
