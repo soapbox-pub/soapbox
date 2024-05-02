80b500e710b0be7eee8a3a8ce3937e416533780b
Alex Gleason <alex@alexgleason.me>
2024-05-02 15:38:01 -0500

    Feature-gate blocks

--- src/utils/features.ts
+++ src/utils/features.ts
@@ -252,6 +252,14 @@ const getInstanceFeatures = (instance: Instance) => {
     blockersVisible: features.includes('blockers_visible'),
 
     /**
+     * Ability to block users.
+     * @see POST /api/v1/accounts/:id/block
+     * @see POST /api/v1/accounts/:id/unblock
+     * @see GET /api/v1/blocks
+     */
+    blocks: v.software !== DITTO,
+
+    /**
      * Can group bookmarks in folders.
      * @see GET /api/v1/pleroma/bookmark_folders
      * @see POST /api/v1/pleroma/bookmark_folders
