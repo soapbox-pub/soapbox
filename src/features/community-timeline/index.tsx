c7ff472ae0f1f0199c258b9192ff9009c73a185a
Alex Gleason <alex@alexgleason.me>
2024-06-06 21:37:57 -0500

    Use instance domain for local timeline

--- src/features/community-timeline/index.tsx
+++ src/features/community-timeline/index.tsx
@@ -1,23 +1,19 @@
 import React, { useEffect } from 'react';
-import { defineMessages, FormattedMessage, useIntl } from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 
 import { expandCommunityTimeline } from 'soapbox/actions/timelines';
 import { useCommunityStream } from 'soapbox/api/hooks';
 import PullToRefresh from 'soapbox/components/pull-to-refresh';
 import { Column } from 'soapbox/components/ui';
-import { useAppSelector, useAppDispatch, useSettings, useTheme } from 'soapbox/hooks';
+import { useAppSelector, useAppDispatch, useSettings, useTheme, useInstance } from 'soapbox/hooks';
 import { useIsMobile } from 'soapbox/hooks/useIsMobile';
 
 import Timeline from '../ui/components/timeline';
 
-const messages = defineMessages({
-  title: { id: 'column.community', defaultMessage: 'Local timeline' },
-});
-
 const CommunityTimeline = () => {
-  const intl = useIntl();
   const dispatch = useAppDispatch();
   const theme = useTheme();
+  const instance = useInstance();
 
   const settings = useSettings();
   const onlyMedia = settings.community.other.onlyMedia;
@@ -41,7 +37,7 @@ const CommunityTimeline = () => {
   }, [onlyMedia]);
 
   return (
-    <Column className='-mt-3 sm:mt-0' label={intl.formatMessage(messages.title)} transparent={!isMobile}>
+    <Column className='-mt-3 sm:mt-0' label={instance.domain} transparent={!isMobile}>
       <PullToRefresh onRefresh={handleRefresh}>
         <Timeline
           className='black:p-4 black:sm:p-5'
