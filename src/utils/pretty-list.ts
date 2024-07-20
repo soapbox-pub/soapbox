6ec0958617a5820fd2d7d0bbf53066bbcad11329
Siddharth Singh <me@shantaram.xyz>
2024-07-21 03:48:27 +0530

    allow looking up account in relay via /api/v1/admin/ditto/lookup

---
+++ src/utils/pretty-list.ts
@@ -0,0 +1,12 @@
+export const prettyList = (items: string[]): string => {
+  if (items.length === 0) {
+    return '';
+  }
+  if (items.length === 1) {
+    return items[0];
+  }
+  if (items.length === 2) {
+    return `${items[0]} and ${items[1]}`;
+  }
+  return `${items.slice(0, -1).join(', ')}, and ${items[items.length - 1]}`;
+};
