902a13be8fa0889c3987bed0c5fd74a6b03e4287
danidfra <danielfilemondev@gmail.com>
2024-08-15 14:18:18 -0300

    Delete onboarding-wizard route

--- src/features/ui/util/async-components.ts
+++ src/features/ui/util/async-components.ts
@@ -121,7 +121,6 @@ export const SettingsStore = lazy(() => import('soapbox/features/developers/sett
 export const TestTimeline = lazy(() => import('soapbox/features/test-timeline'));
 export const ServiceWorkerInfo = lazy(() => import('soapbox/features/developers/service-worker-info'));
 export const DatePicker = lazy(() => import('soapbox/features/birthdays/date-picker'));
-export const OnboardingWizard = lazy(() => import('soapbox/features/onboarding/onboarding-wizard'));
 export const CompareHistoryModal = lazy(() => import('soapbox/features/ui/components/modals/compare-history-modal'));
 export const AuthTokenList = lazy(() => import('soapbox/features/auth-token-list'));
 export const FamiliarFollowersModal = lazy(() => import('soapbox/features/ui/components/modals/familiar-followers-modal'));
