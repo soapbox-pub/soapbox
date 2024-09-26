66bc8ae30e5dfa6b9ceb1cb34371e5882da88df1
Alex Gleason <alex@alexgleason.me>
2024-09-25 19:51:34 -0500

    Fix type errors in tests

--- src/features/ui/components/modals/landing-page-modal.test.tsx
+++ src/features/ui/components/modals/landing-page-modal.test.tsx
@@ -7,18 +7,18 @@ import LandingPageModal from './landing-page-modal';
 
 describe('<LandingPageModal />', () => {
   it('successfully renders', () => {
-    render(<LandingPageModal onClose={vi.fn} />);
+    render(<LandingPageModal onClose={() => {}} />);
     expect(screen.getByTestId('modal')).toBeInTheDocument();
   });
 
   it('doesn\'t display the signup button by default', () => {
-    render(<LandingPageModal onClose={vi.fn} />);
+    render(<LandingPageModal onClose={() => {}} />);
     expect(screen.queryByText('Register')).not.toBeInTheDocument();
   });
 
   describe('with registrations enabled', () => {
     it('displays the signup button', () => {
-      render(<LandingPageModal onClose={vi.fn} />, undefined, storeOpen);
+      render(<LandingPageModal onClose={() => {}} />, undefined, storeOpen);
       expect(screen.getByText('Register')).toBeInTheDocument();
     });
   });
