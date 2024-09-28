6d96d06a42145a6617c8ae1a3dedc618417781dd
danidfra <danielfilemondev@gmail.com>
2024-09-28 20:20:57 -0300

    Update dropdown icon color

--- src/components/ui/language-dropdown/language-dropdown.tsx
+++ src/components/ui/language-dropdown/language-dropdown.tsx
@@ -58,10 +58,10 @@ const LanguageDropdown = () => {
   return (
     <DropdownMenu items={newMenu} modal>
       { languageIcon === '' ?
-        <SvgIcon src={require('@tabler/icons/outline/world.svg')} className='hover:cursor-pointer hover:text-gray-600 black:absolute black:right-0 black:top-4 black:text-white black:hover:text-gray-600 sm:mr-4' />
+        <SvgIcon src={require('@tabler/icons/outline/world.svg')} className='text-gray-700 hover:cursor-pointer hover:text-gray-500 black:absolute black:right-0 black:top-4 black:text-white black:hover:text-gray-600 sm:mr-4' />
 
         :
-        <button type='button' className='flex h-full rounded-lg border-2 border-black px-1 text-black hover:cursor-pointer hover:border-gray-600 hover:text-gray-600 sm:mr-4 dark:border-white dark:text-white dark:hover:border-gray-700' onClick={() => dispatch(openDropdownMenu())}>
+        <button type='button' className='flex h-full rounded-lg border-2 border-gray-700 px-1 text-gray-700 hover:cursor-pointer hover:border-gray-500 hover:text-gray-500 sm:mr-4 dark:border-white dark:text-white dark:hover:border-gray-700' onClick={() => dispatch(openDropdownMenu())}>
           {languageIcon}
         </button>
       }
