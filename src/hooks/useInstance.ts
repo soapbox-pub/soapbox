9d1ee47166e8775383027e45eb506cb2b4c65519
Alex Gleason <alex@alexgleason.me>
2024-10-11 04:26:27 -0500

    Use the instance hook to fetch in SoapboxLoad, also store the data in Redux as a temporary workaround

--- src/hooks/useInstance.ts
+++ src/hooks/useInstance.ts
@@ -1,22 +1,36 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 
 import { useInstanceV1 } from 'soapbox/api/hooks/instance/useInstanceV1';
 import { useInstanceV2 } from 'soapbox/api/hooks/instance/useInstanceV2';
 import { instanceV2Schema, upgradeInstance } from 'soapbox/schemas/instance';
+
+import { useAppDispatch } from './useAppDispatch';
 
 /** Get the Instance for the current backend. */
 export function useInstance() {
   const v2 = useInstanceV2();
   const v1 = useInstanceV1({ enabled: v2.isError });
 
-  const upgradedV1 = useMemo(() => {
-    if (v1.instance) {
+  const instance = useMemo(() => {
+    if (v2.instance) {
+      return v2.instance;
+    } if (v1.instance) {
       return upgradeInstance(v1.instance);
+    } else {
+      return instanceV2Schema.parse({});
     }
-  }, [v1.instance]);
+  }, [v2.instance, v1.instance]);
 
-  const instance = v2.instance ?? upgradedV1 ?? instanceV2Schema.parse({});
   const props = v2.isError ? v1 : v2;
+
+  // HACK: store the instance in Redux for legacy code
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch({
+      type: 'instanceV2/fetch/fulfilled',
+      payload: { instance },
+    });
+  }, [instance]);
 
   return { ...props, instance };
 }
