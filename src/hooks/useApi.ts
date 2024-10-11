fdc03fe610afb0139456e9cc6a459574476014ad
Alex Gleason <alex@alexgleason.me>
2024-10-11 15:34:46 -0500

    useApi: prefer BACKEND_URL, if provided

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,6 +1,7 @@
 import { useMemo } from 'react';
 
 import { MastodonClient } from 'soapbox/api/MastodonClient';
+import * as BuildConfig from 'soapbox/build-config';
 
 import { useAppSelector } from './useAppSelector';
 import { useOwnAccount } from './useOwnAccount';
@@ -9,7 +10,7 @@ export function useApi(): MastodonClient {
   const { account } = useOwnAccount();
   const authUserUrl = useAppSelector((state) => state.auth.me);
   const accessToken = useAppSelector((state) => account ? state.auth.users.get(account.url)?.access_token : undefined);
-  const baseUrl = new URL(account?.url || authUserUrl || location.origin).origin;
+  const baseUrl = new URL(BuildConfig.BACKEND_URL || account?.url || authUserUrl || location.origin).origin;
 
   return useMemo(() => {
     return new MastodonClient(baseUrl, accessToken);
