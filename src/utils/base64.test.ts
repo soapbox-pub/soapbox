f5c9b7d1b251a55600e4441f9f50b02a06291312
Alex Gleason <alex@alexgleason.me>
2024-10-15 16:10:15 -0500

    Fix base64 test

--- src/utils/base64.test.ts
+++ src/utils/base64.test.ts
@@ -1,10 +1,8 @@
-import * as base64 from './base64';
+import { decodeBase64 } from './base64';
 
-describe('base64', () => {
-  describe('decode', () => {
-    it('returns a uint8 array', () => {
-      const arr = base64.decode('dGVzdA==');
-      expect(arr).toEqual(new Uint8Array([116, 101, 115, 116]));
-    });
+describe('decodeBase64', () => {
+  it('returns a uint8 array', () => {
+    const arr = decodeBase64('dGVzdA==');
+    expect(arr).toEqual(new Uint8Array([116, 101, 115, 116]));
   });
 });
