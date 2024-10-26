70a8fb1526682921296bec9151ab10fc0e859d01
Alex Gleason <alex@alexgleason.me>
2024-10-26 17:06:50 -0500

    Implement a custom ScrollContext component

---
+++ src/components/scroll-context.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+import { usePrevious } from 'soapbox/hooks';
+
+export type Location<T> = ReturnType<typeof useLocation<T>>;
+
+interface IScrollContext {
+  shouldUpdateScroll(prevLocation: Location<any> | undefined, location: Location<any>): boolean;
+  children: React.ReactNode;
+}
+
+export const ScrollContext: React.FC<IScrollContext> = ({ shouldUpdateScroll, children }) => {
+  const location = useLocation();
+  const prevLocation = usePrevious(location);
+
+  useEffect(() => {
+    if (prevLocation && shouldUpdateScroll(prevLocation, location)) {
+      window.scrollTo(0, 0);
+    }
+  }, [location, shouldUpdateScroll]);
+
+  return children;
+};
