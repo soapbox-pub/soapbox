64e837ca740b3740b6713bc69f0ab2c5fa6e1d4f
danidfra <danielfilemondev@gmail.com>
2024-11-01 12:12:35 -0300

    Change classname "tombstone" to use only Tailwind

--- src/components/tombstone.tsx
+++ src/components/tombstone.tsx
@@ -21,7 +21,7 @@ const Tombstone: React.FC<ITombstone> = ({ id, onMoveUp, onMoveDown }) => {
     <HotKeys handlers={handlers}>
       <div className='h-16'>
         <div
-          className='focusable flex h-[42px] items-center justify-center rounded-lg border-2 border-gray-200 text-center dark:border-gray-800'
+          className='flex h-[42px] items-center justify-center rounded-lg border-2 border-gray-200 text-center focus-within:outline-none focus-within:ring-2 focus-within:ring-primary-300 focus:outline-none focus:ring-2 focus:ring-primary-300 dark:border-gray-800'
         >
           <Text theme='muted'>
             <FormattedMessage
