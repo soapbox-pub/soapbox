6a44582c9c4154c0e7eb1dbb7571538dccae4985
P. Reis <patrickpereirareal1@gmail.com>
2024-12-11 15:38:38 -0300

    refactor: add documentation to useBookmarks hook

--- src/api/hooks/statuses/useBookmarks.ts
+++ src/api/hooks/statuses/useBookmarks.ts
@@ -4,6 +4,12 @@ import { useApi } from 'soapbox/hooks/useApi.ts';
 import { useFeatures } from 'soapbox/hooks/useFeatures.ts';
 import { statusSchema } from 'soapbox/schemas/status.ts';
 
+/**
+ * Get all the statuses the user has bookmarked.
+ * https://docs.joinmastodon.org/methods/bookmarks/#get
+ * GET /api/v1/bookmarks
+ * TODO: add 'limit'
+ */
 function useBookmarks() {
   const api = useApi();
   const features = useFeatures();
