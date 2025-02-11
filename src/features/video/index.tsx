bea2c87a09413988153f9b619580524f79df0905
danidfra <danielfilemondev@gmail.com>
2025-02-11 14:40:08 -0300

    Fix responsive issues

--- src/features/video/index.tsx
+++ src/features/video/index.tsx
@@ -533,7 +533,7 @@ const Video: React.FC<IVideo> = ({
   return (
     <div
       role='menuitem'
-      className={clsx('relative box-border flex max-w-full overflow-hidden rounded-[10px] bg-black text-white focus:outline-0', { 'w-full h-full m-0': fullscreen })}
+      className={clsx('relative box-border flex h-screen max-w-full items-center justify-center overflow-hidden rounded-[10px] bg-black text-white focus:outline-0', { 'w-full h-full m-0': fullscreen })}
       style={playerStyle}
       ref={player}
       onClick={handleClickRoot}
