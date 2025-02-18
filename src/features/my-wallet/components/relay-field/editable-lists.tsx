0ed0dcce09592c2ea6e5161122dbf291456566f4
danidfra <danielfilemondev@gmail.com>
2025-02-17 23:18:57 -0300

    Create a field for adding mints while creating wallet

---
+++ src/features/my-wallet/components/relay-field/editable-lists.tsx
@@ -0,0 +1,58 @@
+import HStack from 'soapbox/components/ui/hstack.tsx';
+import Input from 'soapbox/components/ui/input.tsx';
+import Streamfield, { StreamfieldComponent } from 'soapbox/components/ui/streamfield.tsx';
+
+interface IEditableList<T> {
+  items: T[];
+  setItems: (items: T[]) => void;
+}
+
+const MintField: StreamfieldComponent<string> = ({ value, onChange }) => {
+  return (
+    <HStack space={2} grow>
+      <Input
+        type='text'
+        outerClassName='w-full grow'
+        value={value}
+        onChange={(e) => onChange(e.currentTarget.value)}
+      />
+    </HStack>
+  );
+};
+
+const MintEditor: React.FC<IEditableList<string>> = ({ items, setItems }) => {
+  const handleAdd = () => setItems([...items, '' ]);
+  const handleRemove = (index: number) => {
+    const newItems = [...items];
+    newItems.splice(index, 1);
+    setItems(newItems);
+  };
+
+  return <Streamfield values={items} onChange={setItems} component={MintField} onAddItem={handleAdd} onRemoveItem={handleRemove} />;
+};
+
+const RelayField: StreamfieldComponent<string> = ({ value, onChange }) => {
+  return (
+    <HStack space={2} grow>
+      <Input
+        type='text'
+        outerClassName='w-full grow'
+        value={value}
+        onChange={(e) => onChange(e.currentTarget.value)}
+      />
+    </HStack>
+  );
+};
+
+const RelayEditor: React.FC<IEditableList<string>> = ({ items, setItems }) => {
+  const handleAdd = () => setItems([...items, '']);
+  const handleRemove = (index: number) => {
+    const newItems = [...items];
+    newItems.splice(index, 1);
+    setItems(newItems);
+  };
+
+  return <Streamfield values={items} onChange={setItems} component={RelayField} onAddItem={handleAdd} onRemoveItem={handleRemove} />;
+};
+
+export { RelayEditor, MintEditor };
