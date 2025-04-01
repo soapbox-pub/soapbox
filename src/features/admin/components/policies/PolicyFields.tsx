bbf2c5aa49b8870b54db8e06e3772e4714489347
Siddharth Singh <me@shantaram.xyz>
2025-04-01 13:37:51 +0530

    multivaluebadge improvements

--- src/features/admin/components/policies/PolicyFields.tsx
+++ src/features/admin/components/policies/PolicyFields.tsx
@@ -19,9 +19,9 @@ const MultiValueBadge: FC<{
 }> = ({ value, onRemove }) => {
   return (
     <div className='mb-2 mr-2 inline-flex items-center rounded-full bg-gray-200 px-2 py-1 text-gray-800'>
-      <span className='mr-1'>{value}</span>
-      <button onClick={onRemove} className='text-gray-500 hover:text-gray-700' aria-label={messages.removeValue.defaultMessage}>
-        <Icon src={CloseIcon} className='size-3' />
+      <span className='mr-1 max-w-28 overflow-hidden text-ellipsis' title={String(value)}>{value}</span>
+      <button onClick={onRemove} className='text-gray-600 hover:text-gray-800' aria-label={messages.removeValue.defaultMessage}>
+        <Icon src={CloseIcon} className='size-4' />
       </button>
     </div>
   );
